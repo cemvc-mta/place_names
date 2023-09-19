ee49223cd18d1d8b698c2d9cf5c547899e145046
Justin <jyoston@mta.ca>
2023-09-18 21:02:45 -0300

    Add display name to search results trial 2

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -9,6 +9,6 @@
 {%- assign fields = site.data.config-search -%}
 var store = [ 
 {%- for item in items -%} 
-{ "id": {{ item.objectid | jsonify }}, {% for f in fields %}{{ f.field | jsonify }}: {% if item[f.field] %}{{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }}{% else %}"none"{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %} }{%- unless forloop.last -%},{%- endunless -%}
+{ "id": {{ item.objectid | jsonify }}, {% for f in fields %}{{ f.display_name | jsonify }}, {% for f in fields %}{{ f.field | jsonify }}: {% if item[f.field] %}{{ item[f.field] | normalize_whitespace | replace: '""','"' | jsonify }}{% else %}"none"{% endif %}{% unless forloop.last %},{% endunless %}{% endfor %} }{%- unless forloop.last -%},{%- endunless -%}
 {%- endfor -%}
 ];
